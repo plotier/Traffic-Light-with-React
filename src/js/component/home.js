5dd2bfcea541c541fe015f73d234ee1fa3ea44c5
plotier <plotier15@gmail.com>
2021-06-13 18:10:32 +0000

    con brillo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,9 +3,9 @@ import React from "react";
 export function Home() {
 	return (
 		<div className="semaforo">
-			<div className="luz roja">1</div>
-			<div className="luz amarilla">2</div>
-			<div className="luz verde">3</div>
+			<div className="luz roja brillo"></div>
+			<div className="luz amarilla"></div>
+			<div className="luz verde"></div>
 		</div>
 	);
 }
